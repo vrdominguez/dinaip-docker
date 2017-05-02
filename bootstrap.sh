1b9013ccf4fc2b3392071be808dcd5259b3bb1f8
Víctor Rodríguez <victor@vrdominguez.es>
2017-05-02 20:50:44 +0200

    Updated bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Restore persistent configuration (if exists)
-if [ -f $FILE ]; then
-	echo "Restoring your config file..."
+if [ -f /var/persistent/dinaip.conf ]; then
+	echo "[Bootstrap] Restoring your config file..."
 	/bin/cp -a /var/persistent/dinaip.conf /etc/dinaip.conf
 fi
 
 # Launch dinaip
+echo "[Bootstrap] Starting dinaIP for console..."
 /usr/sbin/dinaip -u ${DHUSER} -p ${DHPASS} -i
 
 # If dinaip started correctly 
@@ -14,8 +15,10 @@ if [ $? -eq 0 ]; then
 	# Send logs to STDOUT and stop if dinaip dies
 	tail --pid=$(pgrep dinaIP) -f /var/log/dinaip.log
 	
-	# Copy config to persistent dir
-	echo "Making a backup of your config file..."
-	mkdir -p /var/persistent/
-	/bin/cp -a /etc/dinaip.conf /var/persistent/dinaip.conf
+	if [ -f /etc/dinaip.conf ]; then
+		# Copy config to persistent dir
+		echo "[Bootstrap] Making a backup of your config file..."
+		mkdir -p /var/persistent/
+		/bin/cp -a /etc/dinaip.conf /var/persistent/dinaip.conf
+	fi
 fi
